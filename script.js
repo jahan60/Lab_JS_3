c4004086e30cfffc3cb751d7d48160546f1bd278
jahan60 <faridajahan.ny@email.com>
2025-12-04 18:11:41 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//lab
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Fuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+
+//3rd part:
+let str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+let cell = "";       
+let cells = [];     
+
+for (let i = 0; i < str.length; i++) {
+  let ch = str[i];
+
+
+
+
+
+}
