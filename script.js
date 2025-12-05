9543ae1e68e5993c72c595a91cba35f7c7ad4f77
jahan60 <faridajahan.ny@email.com>
2025-12-04 19:05:55 -0500

    2nd commit

--- script.js
+++ script.js
@@ -13,16 +13,43 @@ for (let i = 0; i <= 100; i++) {
 
 
 //3rd part:
-let str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+//  Starting CSV code
+let str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
 
-let cell = "";       
-let cells = [];     
+// Variables - we using the empty string so that we can concatenate
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+
+let commas = 0;
 
 for (let i = 0; i < str.length; i++) {
-  let ch = str[i];
+  let current = str[i];
 
+  if (current == ",") {
+    commas++;
+  } else if (current == "\n") {
+    console.log(cell1, cell2, cell3, cell4);
+    cell1 = '';
+    cell2 = '';
+    cell3 = '';
+    cell4 = '';
 
+    commas = 0;
+  } else {
+    if (commas == 0) {
+      cell1 += current;
+    } else if (commas == 1) {
+      cell2 += current;
+    } else if (commas == 3) {
+      cell3 += current;
+    } else {
+      cell4 += current;
+    }
+  }
 
-
-
+  if(i + 1 == str.length){
+    console.log(cell1, cell2, cell3, cell4);
+  }
 }
